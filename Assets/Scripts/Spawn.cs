1fa16458b150fa24f6bdd8594d3655cec1421f09
seonxx <seungeon19@ewhain.net>
2022-06-07 21:52:10 +0900

    setting2

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -6,38 +6,39 @@ public class Spawn : MonoBehaviour
 {
     public BoxCollider[] colliders; //랜덤 스폰할 콜라이더
     public GameObject[] prefabs;    //게임 오브젝트
-
+    public ItemDatabase items;
 
     void Start()
     {
         SpawnObjects();
+        for(int i = 0; i < colliders.Length; i++)
+            colliders[i].enabled = false;
     }
 
     private void SpawnObjects()
     {
-        for(int i = 0; i < prefabs.Length; i++)
+        for(int i = 0; i < items.itemDB.Count; i++)
         {
             int select_area = Random.Range(0, colliders.Length);
 
             Vector3 pos = GetRandomPosition(select_area);
-            Instantiate(prefabs[i], pos, Quaternion.identity);
-
+            GameObject go = Instantiate(prefabs[i], pos, Quaternion.identity);
+            go.GetComponent<FieldItems>().SetItem(items.itemDB[i]);
+            GameObject child = Instantiate(go.GetComponent<FieldItems>().item.itemPrefab, pos, Quaternion.identity);
+            child.transform.SetParent(go.transform);
         }
     }
 
     private Vector3 GetRandomPosition(int s)
     {
         Vector3 base_pos = colliders[s].transform.position; //collider위치
-        Debug.Log("collider : " + base_pos);
         Vector3 size = colliders[s].size;
-        Debug.Log("size: " + size);
 
         float pos_x = base_pos.x + Random.Range(-size.x / 2f, size.x / 2f);
         float pos_y = base_pos.y + Random.Range(-size.y / 2f, size.y / 2f);
         float pos_z = base_pos.z + Random.Range(-size.z / 2f, size.z / 2f);
 
         Vector3 pos = new Vector3(pos_x, pos_y, pos_z);
-        Debug.Log("random pos: " + pos);
         return pos;
     }
 }
--- Assets/Scripts/s/ItemDatabase.cs
+++ Assets/Scripts/s/ItemDatabase.cs
@@ -13,16 +13,18 @@ public class ItemDatabase : MonoBehaviour
 
     //아이템 리스트
     public List<Item> itemDB = new List<Item>();
-    [Space(9)]
-    public GameObject fieldItemPrefab; //필드아이템 복제(생성) 위해
-    public Vector3[] pos; //아이템 생성될 위치
+    //[Space(9)]
+    //public GameObject fieldItemPrefab; //필드아이템 복제(생성) 위해
+    //public Vector3[] pos; //아이템 생성될 위치
 
+    /*
     private void Start()
     {
         for(int i = 0; i<4; i++)
         {
-            GameObject go = Instantiate(fieldItemPrefab, pos[i], Quaternion.identity);
-            go.GetComponent<FieldItems>().SetItem(itemDB[i]); //Random.Range(0,5)
+            GameObject go = Instantiate(fieldItemPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            go.GetComponent<FieldItems>().SetItem(itemDB[Random.Range(0, 5)]); //Random.Range(0,5)
         }
     }
+    */
 }
